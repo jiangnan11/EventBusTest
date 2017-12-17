6668915b504acbac5b6556d150ea85201baf0b71
jiangnan <1126996548@qq.com>
2017-12-17 21:18:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/eventbustest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.eventbustest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
